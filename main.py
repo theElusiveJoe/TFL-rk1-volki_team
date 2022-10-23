2874f7ae0020afeac771f7e18f42cdabf394bd10
Yuri Proskurnikov <qmask26@yandex.ru>
2022-10-23 15:41:00 +0300

    main fucn

---
+++ main.py
@@ -0,0 +1,12 @@
+def main():
+    input_file = open("test.CFG", "r")
+    output_file = open("result", "w")
+
+    test = ""
+    for line in input_file.readlines():
+        test += line
+
+    
+
+if __name__ == "__main__":
+    main()
